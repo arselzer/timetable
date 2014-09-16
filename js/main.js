21d8f039366b55f56fec4b39146a828077b1b9f5
Alexander Selzer <aselzer3@gmail.com>
2014-09-16 09:51:03 +0200

    Improved design, started adding  week detection

--- js/main.js
+++ js/main.js
@@ -2,18 +2,22 @@
  *  Copyright © (c) 2014 Alexander Selzer <aselzer3@gmail.com>
  */
 
-var width = 1000
-var height = 540
+
+var totalWidth = 1000
+var labelWidth = 32
+var width = totalWidth - labelWidth
+// height is dynamic, determined by CSS
 
 var tt = $(".timetable")
 
 $.getJSON("timetable.json", function(data) {
 
-  // TODO check input format
-
-  /* Add needed data to input */
+  /*
+    Add time in minutes to periods
+    Calculate length of period
+  */
   var periods = data.periods.map(function(period) {
-    var t1 = period.time[0]
+    var t1 = period.time[0],
         t2 = period.time[1]
 
     period.start = t1.split(":")[0] * 60 + parseInt(t1.split(":")[1])
@@ -24,49 +28,56 @@ $.getJSON("timetable.json", function(data) {
     return period
   })
 
+  // Calculate time span of timetable
   var dayTime = periods[periods.length-1].end - periods[0].start
 
+  // Calculate physical width of periods
   periods = periods.map(function(period) {
-    period.width = (1000 - 32) / (dayTime / period.length)
+    period.width = width / (dayTime / period.length)
     return period
   })
 
+  // Calculate distances to next periods (break)
   for (var i = 0; i < periods.length - 1; i++) {
-    var thisPeriod = periods[i],
-        nextPeriod = periods[i+1]
+    var period1 = periods[i],
+        period2 = periods[i+1]
 
-    thisPeriod.distanceToNext = (1000 - 32) / (dayTime / (nextPeriod.start - thisPeriod.end))
+    period1.distanceToNext = width / (dayTime / (period2.start - period1.end))
   }
+
+  /*
+    weeks = weeks, days, classes per day
+    periods = period definitions
+    classes = class definitions
+  */
 
   var weeks = data.weeks
   var classes = data.classes
-  var periods = data.periods
+  var weekDates = data.week_dates
+  console.log(weeks)
 
-  var columns = periods.length
+  var currentWeek = getCurrentWeek(weeks, weekDates)//weeks[0] // TODO, make dynamic
 
-  console.log(periods)
-  console.log(dayTime)
+  /* Create timetable header */
 
   var header = $("<div>")
     .addClass("header")
 
   periods.forEach(function(period) {
-    var period = $("<div>")
+    header.append($("<div>")
       .text(period.name)
       .addClass("period")
       .css("width", period.width)
-      .css("margin-right", period.distanceToNext)
-    header.append(period)
+      .css("margin-right", period.distanceToNext))
   })
 
   tt.append(header)
-
-  var currentWeek = weeks[0] // TODO, make dynamic
 
   /* Render Timetable */
 
   for (var key in currentWeek.days) {
     var day = currentWeek.days[key]
+    console.log(day)
 
     // row = one day
     var row = $("<div>")
@@ -77,20 +88,19 @@ $.getJSON("timetable.json", function(data) {
       .addClass("name")
       .text(key))
 
+    /*
+      Classes are transformed to Easy-to-render "sections"
+      Sections are basically lessons.
+    */
     var sections = []
 
-    // append classes to row
-    // spaces are not yet here
     day.classes.forEach(function(cl) {
-      // class type of the class & periods
-
-      // class type
       var classType
       var fullPeriods = []
 
-      classes.forEach(function(c) {
-        if (cl.name === c.name)
-          classType = c
+      classes.forEach(function(definedClass) {
+        if (cl.name === definedClass.name)
+          classType = definedClass
       })
 
       periods.forEach(function(period) {
@@ -101,12 +111,12 @@ $.getJSON("timetable.json", function(data) {
         })
       })
 
-      var width = fullPeriods[fullPeriods.length-1].end - fullPeriods[0].start
+      var sectionWidth = fullPeriods[fullPeriods.length-1].end - fullPeriods[0].start
 
-      cl.class = classType,
-      cl.width = (1000 - 32) /
-      (dayTime / width)
+      cl.class = classType
+      cl.width = width / (dayTime / sectionWidth)
       cl.periods = fullPeriods
+      cl.name = cl.name
 
       sections.push(cl)
     })
@@ -115,24 +125,21 @@ $.getJSON("timetable.json", function(data) {
     for (var i = 0; i < day.classes.length - 1; i++) {
       var curClass = day.classes[i],
           nextClass = day.classes[i+1]
+          console.log(curClass)
 
       curClass.timeToNext = (nextClass.periods[0].start - curClass.periods[curClass.periods.length-1].end)
 
-      curClass.distanceToNext = (1000 - 32) /
-      (dayTime / curClass.timeToNext)
-      console.log(curClass)
+      curClass.distanceToNext = width / (dayTime / curClass.timeToNext)
     }
-
-    /* Classes are transformed to Easy-to-render "sections" */
 
     sections.forEach(function(section) {
       var sectionDiv = $("<div>")
         .css("width", section.width)
+        .css("background-color", section.class.color)
         .addClass("section")
 
       if (section.class) {
         sectionDiv.addClass("class")
-          .css("background-color", section.class.color)
           .css("margin-right", section.distanceToNext)
 
         sectionDiv.append($("<div>")
@@ -150,8 +157,12 @@ $.getJSON("timetable.json", function(data) {
 
       row.append(sectionDiv)
     })
-
     tt.append(row)
   }
-
+}).fail(function(err) {
+  console.log(err)
 })
+
+function getCurrentWeek(weeks) {
+  return weeks[0]
+}
